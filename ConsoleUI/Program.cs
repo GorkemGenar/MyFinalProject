89873b3b2e1889112b0438ea24ef162da591cf4d
GorkemGenar <73362767+GorkemGenar@users.noreply.github.com>
2021-02-08 12:45:48 +0300

    .

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -9,14 +9,38 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
-            ProductManager productManager = new ProductManager(new EfProductDal());
+            ProductTest();
+            //CategoryTest();
 
-            foreach (var product in productManager.GetAll())
+            Console.ReadKey();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+
+            var categories = categoryManager.GetAll();
+
+            foreach (var category in categories)
             {
-                Console.WriteLine(product.ProductName);
+                Console.WriteLine(category.CategoryId + " " + category.CategoryName);
             }
 
-            Console.ReadLine();
+            Console.WriteLine("--------------------------------------------");
+
+            categoryManager.GetById(2);
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var product in productManager.GetProductDetails())
+            {
+                Console.WriteLine(product.ProductName + " | " + product.CategoryName);
+            }
+            
+            Console.WriteLine("--------------------------------------------");
         }
     }
 }
--- Core/Entities/IEntity.cs
+++ Core/Entities/IEntity.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace Entities.Abstract
+namespace Core.Entities
 {
     // IEntity implement eden sınıf, bir veri tabanı tablosudur.
     public interface IEntity

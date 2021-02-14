c9a2a9bef74d3d0cf29223645399d3a38c509ab3
GorkemGenar <73362767+GorkemGenar@users.noreply.github.com>
2021-02-14 14:51:47 +0300

    .

---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        public Result(bool success, string message):this(success)
+        {
+            // Getter' lar ReadOnly'dir. Ve ReadOnly'ler constructor'larda set edilebilir!!
+            Massage = message;
+        }
+
+        public Result(bool success)
+        {
+            Success = success;
+        }
+
+        public bool Success { get; }
+
+        public string Massage { get; }
+    }
+}
